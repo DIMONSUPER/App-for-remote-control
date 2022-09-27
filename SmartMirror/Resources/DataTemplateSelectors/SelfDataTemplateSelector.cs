40cfb81c851f898047acc11f52e85bcd25e36c02
Dmytro Fedchenko <dmytro.fedchenko@headworks.com.ua>
2022-09-27 15:20:05 +0300

    Smart-6_Create_Custom_Tabbed_Page: - Custom tabbed page was implemented

---
+++ SmartMirror/Resources/DataTemplateSelectors/SelfDataTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SmartMirror.Resources.DataTemplateSelectors
+{
+    public class SelfDataTemplateSelector : DataTemplateSelector
+    {
+        private readonly Dictionary<string, DataTemplate> _dataTemplatesDictionary = new();
+
+        #region -- Overrides --
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var key = item.GetType().ToString();
+
+            if (!_dataTemplatesDictionary.ContainsKey(key))
+            {
+                _dataTemplatesDictionary[key] = new DataTemplate(item.GetType());
+            }
+
+            return _dataTemplatesDictionary[key];
+        }
+
+        #endregion
+    }
+}
+
