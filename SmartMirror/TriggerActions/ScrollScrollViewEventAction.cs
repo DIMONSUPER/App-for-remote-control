ff8259ca45a9b80b449f211d59781e1aa3d8e391
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2022-10-11 19:16:29 +0300

    ContentPage to VisualElement

--- SmartMirror/TriggerActions/ScrollScrollViewEventAction.cs
+++ SmartMirror/TriggerActions/ScrollScrollViewEventAction.cs
@@ -1,38 +1,35 @@
-﻿using SmartMirror.Views.Tabs;
+﻿namespace SmartMirror.TriggerActions;
 
-namespace SmartMirror.TriggerActions
+public class ScrollScrollViewEventAction : TriggerAction<VisualElement>
 {
-    public class ScrollScrollViewEventAction : TriggerAction<ContentPage>
+    #region -- Public properties --
+
+    public ScrollView ScrollView { get; set; }
+
+    public double ScrollX { get; set; }
+
+    public double ScrollY { get; set; }
+
+    public bool IsAnimated { get; set; }
+
+    public int IntervalInMs { get; set; } = 150;
+
+    #endregion
+
+    #region -- Overrides --
+
+    protected override void Invoke(VisualElement sender)
     {
-        #region -- Public properties --
-
-        public ScrollView ScrollView { get; set; }
-
-        public double ScrollX { get; set; }
-
-        public double ScrollY { get; set; }
-
-        public bool IsAnimated { get; set; }
-
-        public int IntervalInMs { get; set; } = 150;
-
-        #endregion
-
-        #region -- Overrides --
-
-        protected override void Invoke(ContentPage sender)
+        if (ScrollView is not null)
         {
-            if (ScrollView is not null)
+            Device.StartTimer(TimeSpan.FromMilliseconds(IntervalInMs), () =>
             {
-                Device.StartTimer(TimeSpan.FromMilliseconds(IntervalInMs), () =>
-                {
-                    ScrollView.ScrollToAsync(ScrollX, ScrollY, IsAnimated);
+                ScrollView.ScrollToAsync(ScrollX, ScrollY, IsAnimated);
 
-                    return false;
-                });
-            }
+                return false;
+            });
         }
-
-        #endregion
     }
+
+    #endregion
 }
