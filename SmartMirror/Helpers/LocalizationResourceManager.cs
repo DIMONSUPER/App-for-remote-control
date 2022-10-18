cb19bc25ed5c4b530dad4580844758afe73786fe
DESKTOP-2KF8JSO\andrew <andrii.tantsiura@headworks.com.ua>
2022-10-18 10:16:45 +0300

    SMART-51_Spelling_Mistakes_In_NoInternetConnectionState: - fixed localization.

---
+++ SmartMirror/Helpers/LocalizationResourceManager.cs
@@ -0,0 +1,21 @@
+﻿using SmartMirror.Resources.Strings;
+using System.Globalization;
+
+namespace SmartMirror.Helpers
+{
+    public class LocalizationResourceManager
+    {
+        private LocalizationResourceManager()
+        {
+            Strings.Culture = CultureInfo.CurrentCulture;
+        }
+
+        #region -- Public properties --
+        
+        public static LocalizationResourceManager Instance { get; } = new();
+
+        public object this[string resourceKey] => Strings.ResourceManager.GetObject(resourceKey, Strings.Culture) ?? resourceKey; 
+        
+        #endregion
+    }
+}
