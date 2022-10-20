052f040b32a9805047bf71052f42e1b1938cdc3b
DESKTOP-2KF8JSO\andrew <andrii.tantsiura@headworks.com.ua>
2022-10-20 12:54:54 +0300

    SMART-47_Internet_Loader_Isnt_Displayed_In_Rooms: - fixes of mistakes

--- SmartMirror/Helpers/TaskRepeater.cs
+++ SmartMirror/Helpers/TaskRepeater.cs
@@ -7,9 +7,9 @@
         public static async Task<bool> RepeatAsync(Func<Task<bool>> task, TimeSpan executionTime, int delayInMiliseconds = 100, CancellationToken token = default)
         {
             bool isSuccess = false;
-            var timeToStopExection = DateTime.UtcNow + executionTime;
+            var timeToStopExecution = DateTime.UtcNow + executionTime;
 
-            while (!isSuccess && DateTime.UtcNow < timeToStopExection && !token.IsCancellationRequested)
+            while (!isSuccess && DateTime.UtcNow < timeToStopExecution && !token.IsCancellationRequested)
             {
                 isSuccess = await task();
 
