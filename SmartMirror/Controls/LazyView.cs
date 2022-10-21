31ed96833224f94522359fefd1dd70814b0cfb79
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2022-10-21 17:59:14 +0300

    SMART-40 implementation TView

---
+++ SmartMirror/Controls/LazyView.cs
@@ -0,0 +1,21 @@
+﻿namespace SmartMirror.Controls
+{
+    public class LazyView<TView> : ContentView where TView : View, new ()
+    {
+        private bool _isPageLoaded;
+
+        #region -- Public helpers --
+
+        public void TryLoadView()
+        {
+            if (!_isPageLoaded)
+            {
+                Content = new TView { BindingContext = BindingContext };
+
+                _isPageLoaded = true;
+            }
+        }
+
+        #endregion
+    }
+}
