4ae2ce70f4fa475f2f1a974679c7d682ca163459
Dmytro Fedchenko <dmytro.fedchenko@headworks.com.ua>
2022-10-27 10:10:02 +0300

    - Some tabbed page renderer refactoring

--- SmartMirror/Platforms/Android/Renderers/CustomTabbedPageRenderer.cs
+++ SmartMirror/Platforms/Android/Renderers/CustomTabbedPageRenderer.cs
@@ -68,8 +68,6 @@ public class CustomTabbedPageRenderer : TabbedPageRenderer
         {
             PageController.ContainerArea = new(0, tabsHeight, Context.FromPixels(width), Context.FromPixels(height) - tabsHeight);
 
-            SetNavigationRendererPadding(tabsHeight, 0);
-
             Pager.Layout(0, nativeTabsHeight, width, b);
 
             _tabBarView.Measure(MeasureSpec.MakeMeasureSpec(width, MeasureSpecMode.AtMost), MeasureSpec.MakeMeasureSpec(nativeTabsHeight, MeasureSpecMode.AtMost));
@@ -128,18 +126,6 @@ public class CustomTabbedPageRenderer : TabbedPageRenderer
         }
 
         return res;
-    }
-
-    private void SetNavigationRendererPadding(int paddingTop, int paddingBottom)
-    {
-        for (var i = 0; i < PageController.InternalChildren.Count; i++)
-        {
-            if (PageController.InternalChildren[i] is VisualElement child
-                && Microsoft.Maui.Controls.Compatibility.Platform.Android.Platform.GetRenderer(child) is NavigationPageRenderer navigationRenderer)
-            {
-                navigationRenderer.SetPadding(navigationRenderer.PaddingLeft, paddingTop, navigationRenderer.PaddingRight, PaddingBottom);
-            }
-        }
     }
 
     #endregion
