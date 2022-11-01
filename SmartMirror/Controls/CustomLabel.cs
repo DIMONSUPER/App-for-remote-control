f069df171eeab3755f39c8fe854fc7c5b265db79
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2022-11-01 17:09:41 +0200

    SMART-63 fixed bug

---
+++ SmartMirror/Controls/CustomLabel.cs
@@ -0,0 +1,29 @@
+﻿namespace SmartMirror.Controls
+{
+    public class CustomLabel : Label
+    {
+        public CustomLabel()
+        {
+            AppendToMapping();
+        }
+
+        #region -- Private helpers --
+
+        private void AppendToMapping()
+        {
+            Label.ControlsLabelMapper.AppendToMapping(nameof(Label.MaxLines), UpdateMaxLines);
+            Label.ControlsLabelMapper.AppendToMapping(nameof(Label.LineBreakMode), UpdateMaxLines);
+        }
+
+        private void UpdateMaxLines(Microsoft.Maui.Handlers.LabelHandler handler, ILabel label)
+        {
+            var textView = handler.PlatformView;
+            if (label is Label controlsLabel && textView.Ellipsize == Android.Text.TextUtils.TruncateAt.End)
+            {
+                textView.SetMaxLines(controlsLabel.MaxLines);
+            }
+        }
+
+        #endregion
+    }
+}
