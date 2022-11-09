8cc163a024e775822f1aafb170d077d713b2ae5a
Dmytro Fedchenko <dmytro.fedchenko@headworks.com.ua>
2022-11-09 09:32:29 +0200

    SMART-113_Switches_Arent_Switched: - Notification time format was changed to 24h - Notification duplicates were fixed - Notification Status was implemented

--- SmartMirror/Models/BindableModels/NotificationGroupItemBindableModel.cs
+++ SmartMirror/Models/BindableModels/NotificationGroupItemBindableModel.cs
@@ -1,9 +1,15 @@
-﻿using SmartMirror.Interfaces;
+﻿using System.ComponentModel;
+using SmartMirror.Enums;
+using SmartMirror.Interfaces;
+using SmartMirror.Resources;
+using SmartMirror.Resources.Strings;
 
 namespace SmartMirror.Models.BindableModels
 {
     public class NotificationGroupItemBindableModel : BindableBase, INotificationGroupItemModel
     {
+        #region -- Public helpers --
+
         private int _id;
         public int Id
         {
@@ -45,5 +51,51 @@ namespace SmartMirror.Models.BindableModels
             get => _lastActivityTime;
             set => SetProperty(ref _lastActivityTime, value);
         }
+
+        #endregion
+
+        #region -- Overrides --
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+
+            if (args.PropertyName is nameof(Status) or nameof(Device))
+            {
+                StatusFormatted = GetStatusFormatted();
+            }
+        }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private string GetStatusFormatted()
+        {
+            string result = null;
+
+            if (!string.IsNullOrWhiteSpace(Status) && Device is not null)
+            {
+                result = Device.IconSource switch
+                {
+                    IconsNames.pic_humidity => $"{Strings.ChangedTo} {double.Parse(Status) / 100} %",
+                    IconsNames.pic_pressure => $"{Math.Round(double.Parse(Status) / 1000, 2)} kPa",
+                    IconsNames.pic_temperature => $"{Strings.ChangedTo} " + (Device.UnitMeasure == EUnitMeasure.Celsius ? $"{double.Parse(Status) / 100} ℃" : string.Format("{0:F2}", double.Parse(Status) / 100 * 1.8 + 32) + " ℉"),
+                    IconsNames.pic_wall_switch_double_left => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_wall_switch_double_right => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_wall_switch_single => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_wall_switch_three_center => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_wall_switch_three_left => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_wall_switch_three_right => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_motion => Status is "1" ? Strings.TurnedOn : Strings.TurnedOff,
+                    IconsNames.pic_dimmer => $"{Strings.ChangedTo} {Status} lux",
+                    _ => Status,
+                };
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
